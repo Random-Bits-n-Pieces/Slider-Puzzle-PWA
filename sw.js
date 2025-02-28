a8fa2056e9da591bd2cc5721dcea631feefcf861
developer-badfilerequest <developer@badfilerequest.net>
2025-02-28 10:38:21 +1000

    Update layout

--- sw.js
+++ sw.js
@@ -1,21 +1,22 @@
 const CACHE_NAME = `slider-puzzle-pwa`
+const CACHE_CONTENT = [
+  '/',
+  '/imgs/Tiles0.jpg',
+  '/imgs/Tiles1.jpg',
+  '/imgs/Tiles2.jpg',
+  '/imgs/Tiles3.jpg',
+  '/imgs/Tiles4.jpg',
+  '/imgs/Tiles5.jpg',
+  '/imgs/Tiles6.jpg',
+  '/main.js',
+  '/styles.css'
+];
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
-    cache.addAll([
-      '/',
-      '/imgs/Tiles0.jpg',
-      '/imgs/Tiles1.jpg',
-      '/imgs/Tiles2.jpg',
-      '/imgs/Tiles3.jpg',
-      '/imgs/Tiles4.jpg',
-      '/imgs/Tiles5.jpg',
-      '/imgs/Tiles6.jpg',
-      '/main.js',
-      '/styles.css'
-    ]);
+    cache.addAll(CACHE_CONTENT);
   })());
 });
 
